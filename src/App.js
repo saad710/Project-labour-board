dec873549d96da4bd0810e049741968ce6c1b975
Md. Ifrat Hasnat <ifrathasnat2@gmail.com>
2022-04-28 15:05:21 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import LabourBoard from "../src/components/LabourBoard"
-import { useEffect } from 'react';
-const ZOHO = window.ZOHO
+import logo from "./logo.svg";
+import "./App.css";
+import LabourBoard from "../src/components/LabourBoard";
+import { useEffect, useState } from "react";
+const ZOHO = window.ZOHO;
 
 function App() {
-
+  const [projectData,setProjectData] = useState(null)
+  const [painterData,setPainterData] = useState(null)
+  const [zohoLoaded,setZohoLoaded] = useState(false)
+   
   useEffect(() => {
     ZOHO.embeddedApp.on("PageLoad", function (data) {
-      console.log(data);
       //Custom Bussiness logic goes here
     });
     /*
      * initializing the widget.
      */
-        ZOHO.embeddedApp.init().then(() => {
-        ZOHO.CRM.API.searchRecord({Entity:"FP_Projects",Type:"criteria",Query:"(((Project_Status:equals:Requested)or(Project_Status:equals:In Progress)))"})
-        .then(function(data){
-        console.log(data)
-       
-     
-
-})
-        });
-
-
-
+    ZOHO.embeddedApp.init().then(() => {
+     setZohoLoaded(true);
+    });
   }, []);
 
+  useEffect(()=> {
+ if(zohoLoaded){
+  ZOHO.CRM.API.searchRecord({
+    Entity: "FP_Projects",
+    Type: "criteria",
+    Query:
+      "(((Project_Status:equals:Requested)or(Project_Status:equals:In Progress)))",
+  }).then(function (data) {
+    console.log("Project Data ", data);
+    setProjectData(data?.data)
+    
+  });
+  ZOHO.CRM.API.searchRecord({
+    Entity: "Contractors",
+    Type: "criteria",
+    Query:"(Contractor_Status:equals:Active)",
+  }).then(function (data) {
+    console.log("Painter Data ", data);
+    setPainterData(data?.data)
+  });
+ }
+  }, [zohoLoaded])
 
+      // {/* <>{JSON.stringify(projectData)}{JSON.stringify(painterData)}</> */}
 
   return (
-    <div className="App" style={{padding:"2vh"}} >
-        <LabourBoard/>
+    <div className="App" style={{ padding: "2vh" }}>
+    
+        <LabourBoard projectData={projectData} painterData={painterData} />
+      
     </div>
   );
 }
--- src/components/LabourBoard.jsx
+++ src/components/LabourBoard.jsx
@@ -11,32 +11,50 @@ import { useForm, Controller } from "react-hook-form";
 // import "../../styles/JobCard.css"
 
 const painters = ["Christine", "Alex", "Grey", "Hopper"];
-// const LabourWorkData = [
-//     {Contact : "",Date : "7/12/3",Job:"34567",Client:"furora International",Project:"Library Management"},
-//     {Contact : "",Date : "7/12/3",Job:"35067",Client:"furora International",Project:"Library Management"},
-//     {Contact : "",Date : "7/12/3",Job:"35567",Client:"furora International",Project:"Library Management"},
-//     {Contact : "",Date : "7/12/3",Job:"36067",Client:"furora International",Project:"Library Management"},
-// ]
+const TableHeadRecord = [
 
-// let myObj = {
-//     foo0_hour_0: 'test',
-//     foo1_hour_0: 'test',
-//     foo2: 'test',
-//     foo3: 'test',
-//   };
-
-//   const res = Object.keys(myObj).filter(([c]) => c == "[a-zA-Z]+_0-9");
-//   console.log(res);
-// let filtered_keys = (obj, filter) => {
-//     let key, keys = []
-//     for (key in obj)
-//       if (obj.hasOwnProperty(key) && filter.test(key))
-//         keys.push(key)
-//     return keys
-//   }
-
-//   // example:
-//   let filteredNames = filtered_keys(myObj, /hour_1/)
+  {
+    field_name: "Date",
+    api_name: "Es",
+  },
+  {
+    field_name: "Job no",
+    api_name: "Job_Book_Number",
+  },
+  {
+    field_name: "Client",
+    api_name: "Account_Name",
+  },
+  {
+    field_name: "Project",
+    api_name: "Name",
+  },
+  // {
+  //   field_name: "Status",
+  //   api_name: "Project_Status",
+  // },
+  {
+    field_name: "Office Budget",
+    api_name: "Budget_hours",
+  },
+  {
+    field_name: "Assess",
+    api_name: "Painter_Estimate",
+  },
+  {
+    field_name: "WIP",
+    api_name: "WIP",
+  },
+  {
+    field_name: "To Complete",
+    api_name: "To_Complete",
+  },
+  {
+    field_name: "Status",
+    api_name: "Project_status",
+  },
+  
+]
 
 const LabourWorkData = [
   {
@@ -109,12 +127,56 @@ const LabourWorkData = [
   },
 ];
 
-const LabourBoard = () => {
+const LabourBoard = (props) => {
+  const {projectData,painterData} = props;
   const { register, control, watch, handleSubmit, setValue } = useForm();
+ 
+  console.log(projectData)
 
   const allfields = watch();
   console.log(allfields);
   const onSubmit = (data) => console.log(data);
+
+  const handlePainterVal = (e,field) => {
+    console.log(field.name);
+    field.onChange(e.target.value);
+    const grabId = field.name.split("__")[1];
+    console.log(grabId);
+    let assessValue =
+      watch("assess" + "__" + grabId) || 0;
+
+    if (/^\d+$/.test(assessValue)) {
+      assessValue = parseInt(assessValue);
+    } else {
+      assessValue = 0;
+    }
+    // console.log(assessValue)
+
+    console.log(watch());
+    // const allPainters = painterData;
+    // console.log({allPainters})
+
+    let wipValue = 0;
+    let toCompleteValue;
+
+    painterData.forEach((painter) => {
+      const value = watch(painter.First_Name + "__" + grabId);
+
+      const regex = /^\d+$/;
+
+      wipValue += regex.test(value)
+        ? parseInt(value)
+        : 0;
+    });
+    // console.log("assess",assessValue)
+    // console.log("wip",wipValue)
+    toCompleteValue = assessValue - wipValue;
+    console.log({ toCompleteValue });
+    setValue("toComplete__" + grabId, toCompleteValue);
+    setValue("wip__" + grabId, wipValue);
+
+    console.log({ wipValue });
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} style={{width:"98%"}}>
@@ -136,7 +198,7 @@ const LabourBoard = () => {
                 colSpan="4"
                 style={{
                   backgroundColor: "#b19cd9",
-                  "text-align": "center",
+                  "textAlign": "center",
                   fontWeight: "bold",
                   fontSize: "25px",
                 }}
@@ -173,7 +235,7 @@ const LabourBoard = () => {
               <TableCell sx={{ borderLeft: "1px solid black" }}>
                 Status
               </TableCell>
-              {painters?.map((name, index) => (
+              {/* {painterData?.map((painterInfo, index) => (
                 <TableCell
                   sx={{
                     borderLeft: "1px solid black",
@@ -183,32 +245,22 @@ const LabourBoard = () => {
                   }}
                   key={index}
                 >
-                  {name}
+                  {painterInfo.First_Name}
                 </TableCell>
-              ))}
+              ))} */}
             </TableRow>
           </TableHead>
           <TableHead>
-            <TableRow>
+           
+                <TableRow >
               <TableCell>Contact</TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>Date</TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>
-                {" "}
-                Job no
-              </TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>
-                Client
-              </TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>
-                Project
-              </TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>
-                Office Budget
-              </TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}>
-                Asseses
-              </TableCell>
-              <TableCell
+              {
+              TableHeadRecord?.map((headInfo,index) => (
+              <TableCell key={index} sx={{ borderLeft: "1px solid black",backgroundColor:`${headInfo.field_name === "WIP" ? "#addfad" : headInfo.field_name === "To Complete" ? "#b19cd9" : ""  }` }}>{headInfo.field_name}</TableCell>
+          
+              ) )
+              }
+              {/* <TableCell
                 sx={{
                   borderLeft: "1px solid black",
                   backgroundColor: "#addfad",
@@ -224,22 +276,23 @@ const LabourBoard = () => {
               >
                 To Compelte
               </TableCell>
-              <TableCell sx={{ borderLeft: "1px solid black" }}></TableCell>
-              {painters?.map((name, index) => (
+              <TableCell sx={{ borderLeft: "1px solid black" }}></TableCell> */}
+              {painterData?.map((name, index) => (
                 <TableCell
                   sx={{ borderLeft: "1px solid black" }}
                   key={index}
                 ></TableCell>
               ))}
             </TableRow>
+            
           </TableHead>
           <TableBody>
-            {LabourWorkData?.map((data, rowindex) => (
+            {projectData?.map((data, rowindex) => (
               <TableRow key={rowindex}>
                 <TableCell>
                   <Controller
-                    name={"contact" + "__" + data.Job}
-                    defaultValue={data.Contact}
+                    name={"contact" + "__" + data.Job_Number}
+                    // defaultValue={data.Contact}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -256,9 +309,9 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"date" + "__" + data.Job}
+                    name={"date" + "__" + data.Job_Number}
                     control={control}
-                    defaultValue={data.Date}
+                    defaultValue={data.Es || ""}
                     render={({ field }) => (
                       <TextField
                         fullWidth
@@ -274,9 +327,9 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"job" + "__" + data.Job}
+                    name={"job" + "__" + data.Job_Number}
                     control={control}
-                    defaultValue={data.Job}
+                    defaultValue={data.Job_Number || ""}
                     render={({ field }) => (
                       <TextField
                         fullWidth
@@ -292,26 +345,8 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"client" + "__" + data.Job}
-                    defaultValue={data.Client}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        fullWidth
-                        {...field}
-                        id="standard-basic"
-                        variant="standard"
-                        InputProps={{
-                          disableUnderline: true,
-                        }}
-                      />
-                    )}
-                  />
-                </TableCell>
-                <TableCell sx={{ borderLeft: "1px solid black" }}>
-                  <Controller
-                    name={"project" + "__" + data.Job}
-                    defaultValue={data.Project}
+                    name={"client" + "__" + data.Job_Number}
+                    defaultValue={data.Account_name || ""}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -328,7 +363,8 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"officeBudget" + "__" + data.Job}
+                    name={"project" + "__" + data.Job_Number}
+                    defaultValue={data.Name || ""}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -345,7 +381,26 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"assess" + "__" + data.Job}
+                    name={"officeBudget" + "__" + data.Job_Number}
+                    defaultValue={data.Budget_hours ||""}
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        {...field}
+                        id="standard-basic"
+                        variant="standard"
+                        InputProps={{
+                          disableUnderline: true,
+                        }}
+                      />
+                    )}
+                  />
+                </TableCell>
+                <TableCell sx={{ borderLeft: "1px solid black" }}>
+                  <Controller
+                    name={"assess" + "__" + data.Job_Number}
+                    defaultValue={data.Painter_Estimate || ""}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -383,13 +438,13 @@ const LabourBoard = () => {
                   sx={{
                     borderLeft: "1px solid black",
                     backgroundColor:
-                      watch("toComplete__" + [data.Job]) < 0
+                      watch("toComplete__" + [data.Job_Number]) < 0
                         ? "red"
                         : "#addfad",
                   }}
                 >
                   <Controller
-                    name={"wip" + "__" + data.Job}
+                    name={"wip" + "__" + data.Job_Number}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -413,13 +468,13 @@ const LabourBoard = () => {
                   sx={{
                     borderLeft: "1px solid black",
                     backgroundColor:
-                      watch("toComplete__" + [data.Job]) < 0
+                      watch("toComplete__" + [data.Job_Number]) < 0
                         ? "red"
                         : "#addfad",
                   }}
                 >
                   <Controller
-                    name={"toComplete" + "__" + data.Job}
+                    name={"toComplete" + "__" + data.Job_Number}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -437,7 +492,7 @@ const LabourBoard = () => {
                 </TableCell>
                 <TableCell sx={{ borderLeft: "1px solid black" }}>
                   <Controller
-                    name={"status" + "__" + data.Job}
+                    name={"status" + "__" + data.Job_Number}
                     control={control}
                     render={({ field }) => (
                       <TextField
@@ -453,10 +508,10 @@ const LabourBoard = () => {
                     )}
                   />
                 </TableCell>
-                {data.painters?.map((info, index) => (
+                {painterData?.map((info, index) => (
                   <TableCell sx={{ borderLeft: "1px solid black" }} key={index}>
                     <Controller
-                      name={info.name + "__" + data.Job}
+                      name={info.First_Name + "__" + data.Job_Number}
                       control={control}
                       render={({ field }) => (
                         <TextField
@@ -465,44 +520,7 @@ const LabourBoard = () => {
                           id="standard-basic"
                           variant="standard"
                           onChange={(e) => {
-                            console.log(field.name);
-                            field.onChange(e.target.value);
-                            const grabId = field.name.split("__")[1];
-                            console.log(grabId);
-                            let assessValue =
-                              watch("assess" + "__" + grabId) || 0;
-
-                            if (/^\d+$/.test(assessValue)) {
-                              assessValue = parseInt(assessValue);
-                            } else {
-                              assessValue = 0;
-                            }
-                            // console.log(assessValue)
-
-                            console.log(watch());
-                            const allPainters = data.painters;
-                            // console.log({allPainters})
-
-                            let wipValue = 0;
-                            let toCompleteValue;
-
-                            allPainters.forEach((painter) => {
-                              const value = watch(painter.name + "__" + grabId);
-
-                              const regex = /^\d+$/;
-
-                              wipValue += regex.test(value)
-                                ? parseInt(value)
-                                : 0;
-                            });
-                            // console.log("assess",assessValue)
-                            // console.log("wip",wipValue)
-                            toCompleteValue = assessValue - wipValue;
-                            console.log({ toCompleteValue });
-                            setValue("toComplete__" + grabId, toCompleteValue);
-                            setValue("wip__" + grabId, wipValue);
-
-                            console.log({ wipValue });
+                            handlePainterVal(e,field)
                           }}
                           InputProps={{
                             disableUnderline: true,
